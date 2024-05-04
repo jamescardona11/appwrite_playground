ae49e51bb7e19d52bd44909674c9b8f081437c9d
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-05-03 21:48:26 -0500

    add logs in gemini

--- prompt_gemini_js/src/main.js
+++ prompt_gemini_js/src/main.js
@@ -3,16 +3,18 @@ import { system, throwIfMissing } from './utils.js';
 
 
 
-export default async ({ req, res }) => {
+export default async ({ req, res, log, error }) => {
   throwIfMissing(process.env, ['GEMINI_API_KEY']);
 
   if (req.method === 'GET') {
+    log('GET request');
     return res.json({ ok: false, response: '' }, 200);
   }
 
   try {
     throwIfMissing(req.body, ['prompt']);
   } catch (err) {
+    error(err.message);
     return res.json({ ok: false, error: err.message }, 400);
   }
 
@@ -24,11 +26,15 @@ export default async ({ req, res }) => {
 
     const prompt = req.body.prompt;
 
+    log(`Generating content for prompt: ${prompt}`);
+
     const result = await model.generateContent(system + prompt);
     const response = await result.response;
     const text = response.text();
+    log(`Generated content: ${text}`);
     return res.json({ ok: true, response: text }, 200);
   } catch (err) {
+
     return res.json({ ok: false, response: '' }, 200);
   }
 };
