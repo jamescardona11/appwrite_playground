1ca7d16bb2a3785ab8869709f4aeecb1ef9ee29b
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-05-03 22:25:29 -0500

    Fixin dart function

--- prompt_gemini_dart/lib/main.dart
+++ prompt_gemini_dart/lib/main.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 import 'dart:io';
+
 import 'package:google_generative_ai/google_generative_ai.dart';
 
 import 'utils.dart';
-
 
 Future<dynamic> main(final context) async {
   throwIfMissing(Platform.environment, ['GEMINI_API_KEY']);
@@ -18,16 +18,19 @@ Future<dynamic> main(final context) async {
     return context.res.json({'ok': false, 'error': err.toString()});
   }
 
-  const apiKey = Platform.environment['GEMINI_API_KEY']!;
+  var apiKey = Platform.environment['GEMINI_API_KEY']!;
   final prompt = context.req.body['prompt'];
 
   try {
-
+    context.log('Generating content for prompt: $prompt');
     final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
     final response = await model.generateContent([Content.text(system + prompt)]);
 
+    context.log('Generated content: ${response.text}');
+
     return context.res.json({'ok': true, 'response': response.text}, 200);
-  } on RequestFailedException {
-    return context.res.json({'ok': false, 'response', '');
+  } catch (err) {
+    context.log(err.toString());
+    return context.res.json({'ok': false, 'response': ''}, 240);
   }
 }
