9b90b5220b8f02fbfaa9b9587353605ddc1fccb7
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-05-04 21:36:24 -0500

    update prompt for gpt

--- link_processor_py/src/open_ai.py
+++ link_processor_py/src/open_ai.py
@@ -4,7 +4,7 @@ import os
 
 def open_ai(context, link):
 
-    prompt= "Give the summary for the next url; maximum 30 words:" + link
+    prompt= "Give the summary for the next url; maximum 30 words."
 
     openai.api_key = os.environ["OPENAI_API_KEY"]
 
@@ -12,11 +12,12 @@ def open_ai(context, link):
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             max_tokens=int(os.environ.get("OPENAI_MAX_TOKENS", "512")),
-            messages=[{"role": "user", "content": prompt}],
+            messages=[{"role": "system", "content": prompt}, {"role": "user", "content": link}],
             
         )
         completion = response.choices[0].message.content
         return context.res.json({"ok": True, "completion": completion}, 200)
-
-    except Exception:
+    
+    except Exception as e:
+        context.error(e)
         return context.res.json({"ok": False, "error": "Failed to query model."}, 500)
