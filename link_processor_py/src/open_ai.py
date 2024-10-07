7c82e8e8269d6c925f75398c62e2b3c55b6f2468
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-10-07 09:52:25 -0500

    update prompt for gpt

--- link_processor_py/src/open_ai.py
+++ link_processor_py/src/open_ai.py
@@ -8,43 +8,47 @@ def open_ai(context, link, summary):
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "authorization": "Bearer " + api_key   
+        "authorization": "Bearer " + api_key
     }
-    
-    
-    prompt= "Give the summary for the next url; maximum 30 words."
+
+
+    prompt= """You are a web content summarization AI. You will summarize the content of a provided web page, focusing on extracting the most relevant information and key points.
+        - The summary must be concise and limited to 30 lines.
+        - Avoid any phrases that reference the structure of the post, such as "the next post," "the post is," or "this medium post."
+        - Do not include any preamble, commentary, or quotes in the output.
+         Present the summary in a clear and organized manner, ensuring it captures the essence of the content without unnecessary details."""
+
     sP = link
-    
+
     if summary != "":
-        prompt= "Give the summary for next text something short; maximum 30 words."
         sP = summary
-    
+
     context.log("prompt")
     context.log(prompt)
-    
 
-    
+
+
     payload = {
         "model": "gpt-3.5-turbo",
         "messages": [
-            {"role": "system", "content": prompt}, 
+            {"role": "system", "content": prompt},
             {"role": "user", "content": sP}
         ],
     }
 
-    
-    
+
+
 
     try:
         response = requests.post(url, json=payload, headers=headers)
         json = response.json()
         context.log(json)
-        
-        
+
+
         result = json['choices'][0]['message']['content']
-        
+
         return result
-    
+
     except Exception as e:
         context.error(e)
         return ''
